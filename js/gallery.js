034b98e0a56fe9a3c0cdb441bd47e4ab7945edbe
Wasyl Destro <aziehx@o2.pl>
2019-04-17 23:18:47 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,14 @@
-function loadPhotos (folderName) {
-    $.ajax({
-        url: "//azieh.github.io/gallery/foto/" + folderName,
-        success: function(data){
-           $(data).find("td > a").each(function(){
-              // will loop through 
-              alert("Found a file: " + $(this).attr("href"));
-           });
-        }
-      });
+function loadPhotos(folderName) {
+   jQuery.support.cors = true;
+   $.ajax({
+      url: "https://github.com/azieh/gallery/tree/master/foto/" + folderName,
+      success: function (data) {
+         $(data).find("td > a").each(function () {
+            // will loop through 
+            alert("Found a file: " + $(this).attr("href"));
+         });
+      }
+   });
 };
 loadPhotos("berlin");
 baguetteBox.run('.tz-gallery');
