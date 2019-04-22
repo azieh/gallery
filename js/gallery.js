3aeb6005a1d740cfed67e2345efa13c3ea4216be
Wasyl Destro <aziehx@o2.pl>
2019-04-22 12:39:36 +0200

    1

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,31 @@
 Vue.component('one-pic-template', {
-   props: ['pic'],
+   props: {
+      pic: Object
+   },
    template: `
-   <div class="col-sm-{{pic.w}} col-md-{{pic.h}}">
-      <a class="lightbox" href="{{pic.href}}">
-         <img src="{{pic.href}}"">
+   <div class="col-sm-12 col-md-4">
+      <a class="lightbox" v-bind:href="pic.href">
+         <img  v-bind:src="pic.href">
       </a>
    </div>
    `
-})
-
+  
+});
 var galleryVm = new Vue({
    el: '#galleryOne',
    data: {
       gallery: []
-   }
-})
-
-var template = '<div class="row"><div class="col-sm-12 col-md-4"><a class="lightbox" href=""><img src=""></a></div></div>';
+   },
+   updated: function () {
+      baguetteBox.run('.tz-gallery');
+    }
+});
 var mainVm = function () {
    var self = this;
    this.corsProxy = 'https://cors-anywhere.herokuapp.com/';
    this.githubFotoPage = 'https://github.com/azieh/gallery/tree/master/foto/';
    this.galleryFolders = [];
+
    this.loadPhotos = function (folderName) {
       if (folderName == null)
          folderName = ''
@@ -32,12 +36,14 @@ var mainVm = function () {
             'Access-Control-Allow-Headers': 'Origin'
          },
          success: function (data) {
-            self.parseResponse(data);
+            self.parseResponse(data, folderName);
+
+            
          }
       });
    };
 
-   this.parseResponse = function (data) {
+   this.parseResponse = function (data, parent) {
       var html = $.parseHTML(data);
       $(html).find(".js-navigation-open").each(function () {
          var title = $(this).attr("title");
@@ -48,18 +54,15 @@ var mainVm = function () {
             var pic = {
                h: 12,
                w: 4,
-               href: title
+               href: 'https://raw.githubusercontent.com/azieh/gallery/master/foto/' + parent + '/' + title
             }
-            galleryVm.items.push(pic);
+            galleryVm.gallery.push(pic);
+            
          }
       });
-      var template1 = $.parseHTML(template);
-      $(template1).attr("id", "berlin");
-      $('.tz-gallery').append(template1);
    }
    this.initLoad = function () {
       self.loadPhotos();
-      baguetteBox.run('.tz-gallery');
    }
    self.initLoad();
    return self;
